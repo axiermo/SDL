216ab5d4de99a3f577511f8b38df5dadbe1ff3f0
axiermo <axiermopc@gmail.com>
2016-03-10 10:46:13 +0100

    Added the code with the dll
    Also corrected the glitch that you could go  outside from right and
    bottom

--- Intro_to_SDL_handout/main.cpp
+++ Intro_to_SDL_handout/main.cpp
@@ -1,13 +1,18 @@
-// ---------------------------------
+// ----------------------------------------------------------------
+// Awesome simple game with SDL
+// http://wiki.libsdl.org/APIByCategory
+// ----------------------------------------------------------------
+
+#include <stdio.h>
 #include "stdlib.h"
 #include "string.h"
-#include "stdio.h"
+
 #include "SDL\include\SDL.h"
 #include "SDL_image\include\SDL_image.h"
-// TODO 1: Add SDL_mixer library
-#pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib")
-// You need to add the include file (.h)
 #include "SDL_mixer\include\SDL_mixer.h"
+
+// TODO 1: Add SDL_mixer library
+// You need to add the include file (.h)
 // The library (.lib) and make sure all .dll
 // are in the executable's folder. Everything 
 // is in the folder file already.
@@ -15,7 +20,7 @@
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
 #pragma comment( lib, "SDL_image/libx86/SDL2_image.lib" )
-
+#pragma comment (lib, "SDL_mixer/libx86/SDL2_mixer.lib")
 // Globals --------------------------------------------------------
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 768
@@ -55,9 +60,9 @@ struct globals
 	bool fire, up, down, left, right;
 	KEY_STATE* keyboard;
 	// TODO 4:
+	Mix_Music* music;
+	Mix_Chunk* laser;
 	// Add pointers to store music and the laser fx
-	Mix_Music *music;
-	Mix_Chunk *wav;
 	int scroll;
 } g;
 
@@ -83,33 +88,20 @@ void Start()
 
 	// Create mixer --
 	// TODO 2:
-
 	// Initialize the audio library 
 	// Check https://www.libsdl.org/projects/SDL_mixer/docs/SDL_mixer.html#SEC7
-	int music = MIX_INIT_OGG;
-	int initted = Mix_Init(music);
-
-	// start SDL with audio support
-	if (SDL_Init(SDL_INIT_AUDIO) == -1) {
-		printf("SDL_Init: %s\n", SDL_GetError());
-		exit(1);
-	}
-	// open 44.1KHz, signed 16bit, system byte order,
-	//      stereo audio, using 1024 byte chunks
-	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
-		printf("Mix_OpenAudio: %s\n", Mix_GetError());
-		exit(2);
-	}
+	int flags = MIX_INIT_OGG;
+	int initted = Mix_Init(flags);
+	int audio = Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024);
+	
 	// TODO 5:
 	// Load music.ogg and play it. Store the pointer to it.
-	// Load the laser.wav file and store a pointer to it
-
+	
 	g.music = Mix_LoadMUS("music.ogg");
-	g.wav = Mix_LoadWAV("laser.wav");
-
-	Mix_PlayMusic(g.music, -1);
-
-
+	Mix_PlayMusic(g.music,-1);
+	// Load the laser.wav file and store a pointer to it
+	g.laser = Mix_LoadWAV("laser.wav");
+	
 	// Init other vars --
 	g.scroll = 0;
 	g.ship_x = 100;
@@ -125,15 +117,16 @@ void Finish()
 {
 	// TODO 6:
 	// Stop and free the music and fx
+	Mix_FreeChunk(g.laser);
 	Mix_FreeMusic(g.music);
-	Mix_FreeChunk(g.wav);
-
+	
 	// TODO 3:
-	// Close the audio library
-	Mix_CloseAudio();
 
-	while (Mix_Init(0))
+	Mix_CloseAudio();
+	while (Mix_Init(0)){
 		Mix_Quit();
+	}
+	// Close the audio library
 
 	SDL_DestroyTexture(g.background);
 	SDL_DestroyTexture(g.ship);
@@ -151,15 +144,15 @@ bool CheckInput()
 	const Uint8* keys = SDL_GetKeyboardState(NULL);
 
 	// cyle keyboard keys
-	for (int i = 0; i < MAX_KEYS; ++i)
-	{
-		if (keys[i] == 1)
+	for(int i = 0; i < MAX_KEYS; ++i)
+	{	
+		if(keys[i] == 1)
 			g.keyboard[i] = (g.keyboard[i] == KEY_IDLE) ? KEY_DOWN : KEY_REPEAT;
 		else
 			g.keyboard[i] = (g.keyboard[i] == KEY_REPEAT || g.keyboard[i] == KEY_DOWN) ? KEY_UP : KEY_IDLE;
 	}
 
-	if (g.keyboard[SDL_SCANCODE_ESCAPE] == KEY_DOWN)
+	if(g.keyboard[SDL_SCANCODE_ESCAPE] == KEY_DOWN)
 		return false;
 
 	g.up = g.keyboard[SDL_SCANCODE_UP] == KEY_REPEAT;
@@ -179,14 +172,13 @@ void MoveStuff()
 	// Knowing the booleans up/down/left/right
 	// hold true if the player is trying to move in that direction
 	// calc player's next position (stored in g.x and g.y)
-
 	if (g.up == true){
 		if ((g.ship_y - SHIP_SPEED) > 0){
 			g.ship_y -= SHIP_SPEED;
 		}
 	}
 	else if (g.down == true){
-		if ((g.ship_y +SHIP_SPEED < SCREEN_HEIGHT)){
+		if ((g.ship_y + SHIP_SPEED < SCREEN_HEIGHT - 64)){
 			g.ship_y += SHIP_SPEED;
 		}
 	}
@@ -196,32 +188,29 @@ void MoveStuff()
 		}
 	}
 	else if (g.right == true){
-		if (g.ship_x < SCREEN_WIDTH){
+		if (g.ship_x < SCREEN_WIDTH-64){
 			g.ship_x += SHIP_SPEED;
 		}
 	}
-
-	//shoot
-	if (g.fire)
+	if(g.fire)
 	{
 		g.fire = false;
-
-		if (g.last_shot == NUM_SHOTS)
+		Mix_PlayChannel(-1, g.laser, 0);
+		if(g.last_shot == NUM_SHOTS)
 			g.last_shot = 0;
-
-		Mix_PlayChannel(1, g.wav, 0);
 
 		g.shots[g.last_shot].alive = true;
 		g.shots[g.last_shot].x = g.ship_x + 32;
 		g.shots[g.last_shot].y = g.ship_y;
 		g.last_shot++;
+
 	}
 
-	for (int i = 0; i < NUM_SHOTS; ++i)
+	for(int i = 0; i < NUM_SHOTS; ++i)
 	{
-		if (g.shots[i].alive)
+		if(g.shots[i].alive)
 		{
-			if (g.shots[i].x < SCREEN_WIDTH)
+			if(g.shots[i].x < SCREEN_WIDTH)
 				g.shots[i].x += SHOT_SPEED;
 			else
 				g.shots[i].alive = false;
@@ -299,7 +288,7 @@ int main(int argc, char* argv[])
 {
 	Start();
 
-	while (CheckInput())
+	while(CheckInput())
 	{
 		MoveStuff();
 		Draw();
